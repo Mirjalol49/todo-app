3b5b551a018664230c7b315bfebc496cb3433bd7
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2023-02-19 10:59:07 -0800

    language changed

--- app.js
+++ app.js
@@ -115,8 +115,8 @@ function renderTasks(selectedList) {
 
 function renderTaskCount(selectedList) {
     const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length
-    const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
-    listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+    const taskString = incompleteTaskCount === 1 ? "task" : "ta topshiriqlar"
+    listCountElement.innerText = `${incompleteTaskCount} ${taskString} qoldi`
 }
 
 function renderLists() {
